c4afba46815aafc5dcb8f53acc414d57356b66ea
王湘赣 <wangxianggan@hnjing.com>
2018-12-13 10:11:55 +0800

    a

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const proxy = require('http-proxy-middleware')
+
+const config = require('./config.json')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+config.proxys.forEach(item => {
+    app.use(item.path, proxy(item.config))
+})
+
+app.get('/', (req, res) => res.sendfile('./dist/index.html'))
+app.listen(config.port, () => console.log(`listening on port ${config.port}`))
